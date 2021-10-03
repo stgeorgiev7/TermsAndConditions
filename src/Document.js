5b9aeae36ec3efb622572aaa603bff7763055e87
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-10-03 11:25:30 +0300

    with useState

--- src/Document.js
+++ src/Document.js
@@ -1,19 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Document = (props) => { 
+    const [disabledButton, setEnabled] = useState(true);
 
     const handleScroll = (e) => {
         const bottom = e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
         if (bottom) { 
-            document.querySelector('.agree').removeAttribute('disabled');
+            setEnabled(false); 
         };
       };
 
     return(
-        <div className='content' >
+        <div className='content'>
             <h1 className='title'>{props.title}</h1>
             <div style={{overflowY:'scroll', height:400, width:800, textAlign:'justify', padding:20}} onScroll={handleScroll}>{props.content}</div>
-            <button className='agree' disabled style={{margin: 20}}>I agree</button>
+            <button disabled={disabledButton} style={{margin: 20}}>I agree</button>
         </div>  
     );
   };
